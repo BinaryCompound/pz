aa9910a74a94cf4c859feedcbb2e0f22224d093c
Ilya Pozvolenko <BinaryCompound2@gmail.com>
2022-12-17 23:33:15 +0300

    editing

--- java.js
+++ java.js
@@ -2,9 +2,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("hide").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("hide").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 }
